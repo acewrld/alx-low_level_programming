e39b6b8cc2fa05640ab109f6e5ff710f95ffcf03
Ademola Olalere <olalerer2@gmail.com>
2022-09-16 06:43:47 -0700

    Task 1, I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,15 @@
 
 void  print_alphabet(void)
 {
-  char alphabet = 'a';
+	char alphabet = 'a';
+	
+	while (alphabet <= 'z')
+	
+	{
+		_putchar(alphabet);
+		
+		alphabet++;
+	}
 
-  while (alphabet <= 'z')
-    {
-      _putchar(alphabet);
-      alphabet++;
-    }
-  _putchar('\n');
+	_putchar('\n')
 }
